2b3b2cdc1648606966c3a96aef4b467ade70f5f7
cherrimi <briana20rovelli@gmail.com>
2020-04-07 01:43:44 -0400

    Adding AccountLoader, AccountWriter, and AccountDatabase for saving and making accounts

---
+++ src/AccountDatabase.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class AccountDatabase {
+	private static AccountDatabase accountDatabase = null;
+	private static ArrayList<UserAccount> accountList = new ArrayList<UserAccount>();
+	
+	private AccountDatabase() {
+		accountList = AccountLoader.loadAccounts();
+	}
+	
+	public static AccountDatabase getDatabase() {
+		if(accountDatabase == null) {
+			accountDatabase = new AccountDatabase();
+		}
+		return accountDatabase;
+	}
+	
+	public ArrayList<UserAccount> getAccounts() {
+		return accountList;
+	}
+	
+	public void addUserAccount(String firstName, String lastName, String username, String password, String email) {
+		accountList.add(new UserAccount(firstName, lastName, username, password, email));
+		AccountWriter.saveAccounts();
+	}
+}
--- src/UserAccount.java
+++ src/UserAccount.java
@@ -5,6 +5,10 @@ public class UserAccount extends Account {
 	}
 	
 	public UserAccount(String firstName, String lastName, String username, String password, String email) {
-		super(firstName, lastName, username, password, email);
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.password = password;
+		this.email = email;
 	}
 }
